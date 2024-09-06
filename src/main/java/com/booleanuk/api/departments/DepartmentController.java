4272da4b0253a062be7640497260af4e362a13c3
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-06 09:23:59 +0200

    EXTENSION: Implemented working get endpoints.

---
+++ src/main/java/com/booleanuk/api/departments/DepartmentController.java
@@ -0,0 +1,29 @@
+package com.booleanuk.api.departments;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+    private DepartmentRepository repository;
+
+    public DepartmentController() throws SQLException {
+        this.repository = new DepartmentRepository();
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Department> getAll() throws SQLException {
+        return this.repository.getAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Department get(@PathVariable (name = "id") int id) throws SQLException {
+        return this.repository.get(id);
+    }
+}
---
+++ src/main/java/com/booleanuk/api/departments/DepartmentRepository.java
@@ -0,0 +1,92 @@
+package com.booleanuk.api.departments;
+
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.sql.DataSource;
+import javax.xml.transform.Result;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class DepartmentRepository {
+    DataSource dataSource;
+    String dbUser;
+    String dbURL;
+    String dbPassword;
+    String dbDatabase;
+    Connection connection;
+
+    public DepartmentRepository() throws SQLException {
+        this.getDatabaseCredentials();
+        this.dataSource = this.createDataSource();
+        this.connection = this.dataSource.getConnection();
+    }
+
+    public void getDatabaseCredentials() {
+        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            this.dbUser = prop.getProperty("db.user");
+            this.dbURL = prop.getProperty("db.url");
+            this.dbPassword = prop.getProperty("db.password");
+            this.dbDatabase = prop.getProperty("db.database");
+        }   catch (Exception e) {
+            System.out.println("An error occured: " + e);
+        }
+    }
+
+    private DataSource createDataSource() {
+        final String url = "jdbc:postgresql://" + this.dbURL + ":5432/" + this.dbDatabase + "?user=" + this.dbUser + "&password=" + this.dbPassword;
+        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl(url);
+        return dataSource;
+    }
+
+    public List<Department> getAll() throws SQLException {
+        List<Department> allDepartments = new ArrayList<>();
+        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM departments");
+
+        ResultSet results = statement.executeQuery();
+
+        while (results.next()) {
+            Department department = new Department(
+                    results.getInt("id"),
+                    results.getString("name"),
+                    results.getString("location"));
+            allDepartments.add(department);
+        }
+        return allDepartments;
+    }
+
+    public Department get(int id) throws SQLException {
+        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM departments WHERE id = ?");
+        statement.setInt(1, id);
+        ResultSet results = statement.executeQuery();
+        Department department = null;
+
+        if (results.next()){
+            department = new Department(
+                    results.getInt("id"),
+                    results.getString("name"),
+                    results.getString("location"));
+        }
+        if (department == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No departments with that id were found.");
+        }
+
+        return department;
+    }
+
+
+
+
+
+}
