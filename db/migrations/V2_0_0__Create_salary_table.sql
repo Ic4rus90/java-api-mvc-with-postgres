f4487c082d846e556fc10feccddfc4d92a1d3f1b
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-06 08:44:06 +0200

    Changed variable names for salary table.

--- db/migrations/V2_0_0__Create_salary_table.sql
+++ db/migrations/V2_0_0__Create_salary_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS salaries (
     id SERIAL PRIMARY KEY,
     grade TEXT NOT NULL,
-    min_salary INTEGER,
-    max_salary INTEGER
+    min_salary INT,
+    max_salary INT
 );
 
