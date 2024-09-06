9900cb2fcea5a209dca93cdfa79a6e9639dacf39
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-06 09:10:40 +0200

    EXTENSION: Implemented basic class for department.

---
+++ src/main/java/com/booleanuk/api/departments/Department.java
@@ -0,0 +1,38 @@
+package com.booleanuk.api.departments;
+
+
+public class Department {
+    private int id;
+    private String name;
+    private String location;
+
+    public Department(int id, String name, String location){
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getlocation(){
+        return this.location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+}
