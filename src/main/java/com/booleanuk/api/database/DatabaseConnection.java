3f4e7f9be3a1a070f6fd2e049347a201aedf089c
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-06 12:41:58 +0200

    Implemented logging for database and department repository for education purposes. Correct detections from SonarLint in DepartmentRepository.

--- src/main/java/com/booleanuk/api/database/DatabaseConnection.java
+++ src/main/java/com/booleanuk/api/database/DatabaseConnection.java
@@ -5,10 +5,15 @@ import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class DatabaseConnection {
     private String dbUser;
@@ -16,7 +21,18 @@ public class DatabaseConnection {
     private String dbPassword;
     private String dbDatabase;
     @Getter
-    private Connection connection;
+    private final Connection connection;
+    private static final Logger logger = Logger.getLogger(DatabaseConnection.class.getName());
+
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("src/main/java/com/booleanuk/api/database/database.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to initialize logger handler", e);
+        }
+    }
 
     public DatabaseConnection() throws SQLException {
         this.getDatabaseCredentials();
@@ -25,6 +41,8 @@ public class DatabaseConnection {
     }
 
     private void getDatabaseCredentials() {
+        logger.info("Getting database credentials");
+
         try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
             Properties prop = new Properties();
             prop.load(input);
@@ -32,8 +50,9 @@ public class DatabaseConnection {
             this.dbURL = prop.getProperty("db.url");
             this.dbPassword = prop.getProperty("db.password");
             this.dbDatabase = prop.getProperty("db.database");
+            logger.info("Database credentials successfully fetched.");
         }  catch (Exception e) {
-                System.out.println("An error occured: " + e);
+                logger.severe("An error occured: " + e);
         }
     }
 
--- src/main/java/com/booleanuk/api/departments/DepartmentRepository.java
+++ src/main/java/com/booleanuk/api/departments/DepartmentRepository.java
@@ -4,15 +4,32 @@ import com.booleanuk.api.database.DatabaseConnection;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class DepartmentRepository {
     Connection connection;
+
+    private static final Logger logger = Logger.getLogger(DepartmentRepository.class.getName());
+
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("src/main/java/com/booleanuk/api/departments/department.log", true);
+            fileHandler.setLevel(Level.ALL);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+            logger.setLevel(Level.ALL);
+        } catch (IOException e) {
+            String exceptionMessage = "Failed to initialize logger handler" + e.getMessage();
+            logger.severe(exceptionMessage);
+        }
+    }
 
     public DepartmentRepository() throws SQLException {
         this.connection = new DatabaseConnection().getConnection();
@@ -20,89 +37,138 @@ public class DepartmentRepository {
 
     public List<Department> getAll() throws SQLException {
         List<Department> allDepartments = new ArrayList<>();
-        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM departments");
 
-        ResultSet results = statement.executeQuery();
+        logger.info("Attempting to get all departments.");
 
-        while (results.next()) {
-            Department department = new Department(
-                    results.getInt("id"),
-                    results.getString("name"),
-                    results.getString("location"));
-            allDepartments.add(department);
+        try (PreparedStatement statement = this.connection.prepareStatement("SELECT id, name, location FROM departments")){
+            ResultSet results = statement.executeQuery();
+
+            while (results.next()) {
+                Department department = new Department(
+                        results.getInt("id"),
+                        results.getString("name"),
+                        results.getString("location"));
+                allDepartments.add(department);
+            }
+            logger.info("Departments successfully fetched.");
+            return allDepartments;
+        } catch (SQLException e) {
+            String exceptionMessage = "An error occurred while getting all departments: " + e.getMessage();
+            logger.warning(exceptionMessage);
+            throw new SQLException(exceptionMessage, e);
         }
-        return allDepartments;
     }
 
     public Department get(int id) throws SQLException {
-        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM departments WHERE id = ?");
-        statement.setInt(1, id);
-        ResultSet results = statement.executeQuery();
-        Department department = null;
 
-        if (results.next()){
-            department = new Department(
-                    results.getInt("id"),
-                    results.getString("name"),
-                    results.getString("location"));
-        }
-        if (department == null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No departments with that id were found.");
-        }
+        try (PreparedStatement statement = this.connection.prepareStatement("SELECT id, name, location FROM departments WHERE id = ?")) {
+            statement.setInt(1, id);
+            ResultSet results = statement.executeQuery();
+            Department department = null;
 
-        return department;
+            if (results.next()) {
+                department = new Department(
+                        results.getInt("id"),
+                        results.getString("name"),
+                        results.getString("location"));
+            }
+
+            else {
+                logger.info("No departments with that id were found.");
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No departments with that id were found.");
+            }
+            logger.info("Department successfully fetched by id.");
+            return department;
+
+        } catch (SQLException e) {
+            String exceptionMessage = "An error occurred while getting a department by id: " + e.getMessage();
+            logger.info(exceptionMessage);
+            throw new SQLException(exceptionMessage, e);
+        }
     }
 
     public Department add(Department department) throws SQLException {
-        String SQL = "INSERT INTO departments(name, location) VALUES (?, ?)";
-        PreparedStatement statement = this.connection.prepareStatement(SQL, PreparedStatement.RETURN_GENERATED_KEYS);
-        statement.setString(1, department.getName());
-        statement.setString(2, department.getLocation());
+        String sqlString = "INSERT INTO departments(name, location) VALUES (?, ?)";
 
-        int rowsAffected = statement.executeUpdate();
-        int newID = 0;
+        try (PreparedStatement statement = this.connection.prepareStatement(sqlString, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, department.getName());
+            statement.setString(2, department.getLocation());
 
-        if (rowsAffected > 0){
-            try (ResultSet rs = statement.getGeneratedKeys()) {
-                if (rs.next()) {
-                    newID = rs.getInt(1);
-                }
-            } catch (Exception e) {
-                System.out.println("An error occurred: " + e);
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected == 0) {
+                String exceptionMessage = "Department creation failed, no rows affected";
+                logger.warning(exceptionMessage);
+                throw new SQLException(exceptionMessage);
             }
+
+            int newID = getGeneratedKeys(statement);
             department.setId(newID);
-        } else {
-            System.out.println("Something strange happened");
+
+            logger.info("Department successfully added");
+
+            return department;
+
+        } catch (SQLException e) {
+            String exceptionMessage = "An error occurred while attempting to add a department to the database: " + e.getMessage();
+            logger.severe(exceptionMessage);
+            throw new SQLException(exceptionMessage, e);
         }
-        return department;
+    }
+
+    private int getGeneratedKeys(Statement statement) throws SQLException {
+        try (ResultSet rs = statement.getGeneratedKeys()) {
+            if (rs.next()) {
+                return rs.getInt(1);
+            } else {
+                String exceptionMessage = "An error occurred while getting the generated key: ";
+                logger.warning(exceptionMessage);
+                throw new SQLException(exceptionMessage);
+            }
+        }
     }
 
     public Department update(int id, Department department) throws SQLException {
-        String SQL = "UPDATE departments " +
+        String sqlString = "UPDATE departments " +
                 "SET name = ? ," +
                 "location = ? " +
                 "WHERE id = ?";
-        PreparedStatement statement = this.connection.prepareStatement(SQL);
-        statement.setString(1, department.getName());
-        statement.setString(2, department.getLocation());
-        statement.setInt(3, id);
+        try (PreparedStatement statement = this.connection.prepareStatement(sqlString)) {
+            statement.setString(1, department.getName());
+            statement.setString(2, department.getLocation());
+            statement.setInt(3, id);
 
-        statement.executeUpdate();
+            String oldName = this.get(id).getName();
+            statement.executeUpdate();
 
-        return this.get(id);
+            Department updatedDepartment = this.get(id);
+
+            logger.log(Level.INFO, "Department with name \"{0}\" successfully changed name to \"{1}\"", new String[]{oldName, updatedDepartment.getName()});
+
+            return this.get(id);
+
+        } catch (SQLException e) {
+            String exceptionMessage = "An error occurred while attempting to update a department: " + e.getMessage();
+            logger.warning(exceptionMessage);
+            throw new SQLException(exceptionMessage, e);
+        }
     }
 
     public Department delete(int id) throws SQLException {
-        String SQL = "DELETE FROM departments WHERE id = ?";
-        PreparedStatement statement = connection.prepareStatement(SQL);
-        statement.setInt(1, id);
+        String sqlString = "DELETE FROM departments WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sqlString)) {
+            statement.setInt(1, id);
 
-        Department deletedDepartment = this.get(id);
+            Department deletedDepartment = this.get(id);
 
-        int rowsAffected = statement.executeUpdate();
-        if (rowsAffected == 0) {
-            deletedDepartment = null;
+            statement.executeUpdate();
+
+            logger.log(Level.INFO, "Department with name: \"{0}\" deleted.", deletedDepartment.getName());
+            return deletedDepartment;
+        } catch (SQLException e) {
+            String exceptionMessage = "An error occurred when attempting to delete a department: " + e;
+            logger.warning(exceptionMessage);
+            throw new SQLException(exceptionMessage, e);
         }
-        return deletedDepartment;
     }
 }
